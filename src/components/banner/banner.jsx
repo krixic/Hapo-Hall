7fcf3ec1bf4b7d3eec3040b7ec3b2f126a7aad56
Kayden <krixolodeon@gmail.com>
2023-02-01 22:20:55 +1100

    Change

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -3,12 +3,12 @@ import "./banner.css";
 
 export const Banner = () => {
   return (
-    <div class="banner">
-      <div class="home-title">HAPO HALL</div>
-      <div class="home-subtitle">
+    <div className="banner">
+      <div className="home-title">HAPO HALL</div>
+      <div className="home-subtitle">
         Home to the HARRY POGGERZ achievement list
       </div>
-      <div class="home-heading">
+      <div className="home-heading">
         The HAPO hall is a well endorsed achievement list that spans over the
         many games and activities HAPO participates in.
       </div>
